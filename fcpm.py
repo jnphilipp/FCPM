9323113d170713fe6554e3beb2aa2028c20b0b05
jnphilipp <jnphilipp@gmail.com>
2013-02-10 21:05:58 +0100

    Beginning RemCrBlocks.

--- fcpm.py
+++ fcpm.py
@@ -34,7 +34,9 @@ import string
 #		sys.exit(1)
 
 
-next_pair_comp=0
+#\((([0-9]+?)|(.\|[2-9]+?))\)
+
+next_pair=0
 
 
 def first(a, index):
@@ -45,7 +47,8 @@ def first(a, index):
 		if m:
 			return first(a, int(m.group(1)))
 		else:
-			m = re.search('^\(([0-9]+?)\)', a[index])
+			#m = re.search('^\(([0-9]+?)\)', a[index])
+			m = re.search('^\((([0-9]+?)|(.\|[2-9]+?))\)', a[index])
 			if m:
 				return m.group(0)
 			else:
@@ -59,7 +62,8 @@ def last(a, index):
 		if m:
 			return last(a, int(m.group(1)))
 		else:
-			m = re.search('\(([0-9]+?)\)$', a[index])
+			#m = re.search('\(([0-9]+?)\)$', a[index])
+			m = re.search('\((([0-9]+?)|(.\|[2-9]+?))\)$', a[index])
 			if m:
 				return m.group(0)
 			else:
@@ -75,7 +79,8 @@ def left_pop(a, index):
 	if m:
 		left_pop(a, int(m.group(1)))
 
-	m = re.search('^\(([0-9]+?)\)', a[index])
+	#m = re.search('^\(([0-9]+?)\)', a[index])
+	m = re.search('^\((([0-9]+?)|(.\|[2-9]+?))\)', a[index])
 	if m:
 		a[index] = a[index][len(m.group(0)):]
 	else:
@@ -94,7 +99,8 @@ def right_pop(a, index):
 	if m:
 		right_pop(a, int(m.group(1)))
 
-	m = re.search('\(([0-9]+?)\)$', a[index])
+	#m = re.search('\(([0-9]+?)\)$', a[index])
+	m = re.search('\((([0-9]+?)|(.\|[2-9]+?))\)$', a[index])
 	if m:
 		a[index] = a[index][:-len(m.group(0))]
 	else:
@@ -119,9 +125,38 @@ def remove_nonterminal(a, nonterminal):
 	for i in range(len(a)):
 		a[i] = string.replace(a[i], '<' + str(nonterminal) + '>', '')
 
+def remove_prefix(a, index):
+	if a[index] == '':
+		return
+
+	#m = re.search('^' + a[index][0] + '+', a[index])
+	m = re.search('^<([0-9]+?)>', a[index])
+	if m:
+		print 'f'
+		#left_pop(a, int(m.group(1)))
+	else:
+		m = re.search('^(' + a[index][0] + '+)<([0-9]+?)>', a[index])
+		if m:
+			f = first(a, m.group(2))
+			fm = re.search('\((.)\|([2-9]+?)\)', f)
+
+			if fm:
+				if fm.group(1) == a[index][0]:
+					left_pop(a, m.group(2))
+			elif a[index][0] == f:
+				left_pop(a, m.group(2))
+
+		m = re.search('^(' + a[index][0] + '+)|((' + a[index][0] + '+)\(' + a[index] + '\|([2-9]+?)\))', a[index])
+		if m:
+			if m.group(1) and len(m.group(1)) >= 2:
+				a[index] = string.replace(a[index], m.group(1), '(' + a[index][0] + '|' + str(len(m.group(1))) + ')')
+			elif m.group(2):
+				a[index] = string.replace(a[index], m.group(2), '(' + a[index][0] + '|' + str(len(m.group(3)) + int(m.group(4))) + ')')
+
+
 def preprocessing(a, n, nm):
 	for i in range(len(a)):
-		if i == n or i == mn:
+		if i == n or i == nm:
 			continue
 
 		left_pop(a, i)
@@ -131,7 +166,7 @@ def preprocessing(a, n, nm):
 			remove_nonterminal(a, i)
 
 def pair_comp(a, nm, pair):
-	global next_pair_comp
+	global next_pair
 
 	m = re.search('^\(([0-9]+?)\)', pair)
 	if m:
@@ -145,8 +180,8 @@ def pair_comp(a, nm, pair):
 	else:
 		last_pair = pair[-1]
 
-	for j in range(nm + 0):
-		for i in range(nm + 0):
+	for j in range(nm + 1):
+		for i in range(nm + 1):
 			f = first(a, j)
 			b = first_pair + '<' + str(j) + '>'
 			if b in a[i] and last_pair == f:
@@ -157,15 +192,20 @@ def pair_comp(a, nm, pair):
 			if b in a[i] and first_pair == l:
 				right_pop(a, j)
 
-		a[j] = string.replace(a[j], pair, '(' + str(next_pair_comp) + ')')
+		a[j] = string.replace(a[j], pair, '(' + str(next_pair) + ')')
 
-	for i in range(mn):
+	for i in range(nm):
 		if a[i] == '':
 			remove_nonterminal(a, i)
 
-	next_pair_comp += 1
+	next_pair += 1
 
-def rem_cr_blocks(a, n, nm)
+def rem_cr_blocks(a, n, nm):
+	for i in range(len(a)):
+		if i == n or i == nm:
+			continue
+
+		remove_prefix(a, i)
 
 
 def print_rules(a):
@@ -213,6 +253,38 @@ def gram1():
 	print '########################################'
 
 
+def gram2():
+	a = []
+	a.append('aa')
+	a.append('<0>acca<0>')
+	a.append('bb')
+	a.append('<2>b<2>')
+	a.append('<1><3>')
+	a.append('bbbb')
+	a.append('aaccaaa')
+	a.append('<5>a<6>')
+	a.append('<5>b<7>')
+	a.append('<8>a<6>')
+	a.append('<9>bbb<8>')
+
+	#print '########################################'
+	#print val(a, 4)
+	#print val(a, 10)
+	#print_rules(a)
+
+	preprocessing(a, 4, 10)
+	#print_rules(a)
+
+	rem_cr_blocks(a, 4, 10)
+	print_rules(a)
+
+	print '########################################'
+	#print val(a, 4)
+	#print val(a, 10)
+	#print '########################################'
+
+
 if __name__ == "__main__":
 	#main(sys.argv[1:])
-	gram1()
+	#gram1()
+	gram2()
